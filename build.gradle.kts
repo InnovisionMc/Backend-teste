2dcefa81b27db63dc6687e80f21af76b21010712
InnovisionMc <138728996+InnovisionMc@users.noreply.github.com>
2023-07-27 21:50:52 -0300

    teste

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    groovy
+    java
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
 }
 
-group = "com.example"
+group = "com.ipizza"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -16,8 +16,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.apache.groovy:groovy")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ src/test/java/com/ipizza/ipizza/IpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipizza.ipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
